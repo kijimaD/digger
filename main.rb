adb3be6dec9dc9bb615db703e71da034e961cb8c
kijima <norimaking777@gmail.com>
2021-06-20 23:29:39 +0900

    Add spec files

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ require_pattern = File.join(root_dir, '**/*.rb')
 # Dynamically require everything
 Dir.glob(require_pattern).each do |f|
   next if f.end_with?('/main.rb')
+  next if f.include?('/spec')
 
   require_relative f.gsub("#{root_dir}/", '')
 end
---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+# require_relative '../main'
+
+# RSpec.describe Main do
+#   it 'Can finish game' do
+#     # Main
+#     # main
+#     # exit
+#   end
+# end
