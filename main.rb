20d5f454692cd63ba4c52399b7d436a414143c2d
kijima <norimaking777@gmail.com>
2021-06-20 23:31:11 +0900

    Add

--- main.rb
+++ main.rb
@@ -11,28 +11,31 @@ Dir.glob(require_pattern).each do |f|
   require_relative f.gsub("#{root_dir}/", '')
 end
 
-x = 1
-y = 1
+$test_run = true if ARGV[0] == '--test-run'
 
-while (c = $stdin.getch)
-  puts "\e[H\e[2J" # clear display
+class Main
+  x = 1
+  y = 1
 
-  case c
-  when 'w'
-    y -= 1
-  when 'a'
-    x -= 1
-  when 's'
-    y += 1
-  when 'd'
-    x += 1
-  when 'c'
-    exit
-  end
+  while (c = $stdin.getch)
+    puts "\e[H\e[2J" # clear display
 
-  puts "#{x}, #{y}"
+    case c
+    when 'w'
+      y -= 1
+    when 'a'
+      x -= 1
+    when 's'
+      y += 1
+    when 'd'
+      x += 1
+    when 'c'
+      exit
+    end
 
-  map = <<ROOM
+    puts "#{x}, #{y}"
+
+    map = <<ROOM
   #---#
   |###|
   |###|
@@ -40,7 +43,12 @@ while (c = $stdin.getch)
   #---#
 ROOM
 
-  plane = map.gsub(' ', '').split("\n").map(&:chars)
-  plane[y][x] = '@'
-  puts plane.map { |row| row }.map(&:join).join("\n")
+    plane = map.gsub(' ', '').split("\n").map(&:chars)
+    plane[y][x] = '@'
+    puts plane.map { |row| row }.map(&:join).join("\n")
+
+    exit if $test_run
+  end
 end
+
+main.new
