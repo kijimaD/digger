f2774191d6bcbfdd82463b8447780fe0c6c6a2f0
kijima <norimaking777@gmail.com>
2021-06-20 21:05:56 +0900

    Remove main function

--- main.rb
+++ main.rb
@@ -1,28 +1,37 @@
 require 'io/console'
 
-def main
-  x = 1
-  y = 1
+root_dir = File.dirname(__FILE__)
+require_pattern = File.join(root_dir, '**/*.rb')
 
-  while (c = $stdin.getch)
-    puts "\e[H\e[2J" # clear display
+# Dynamically require everything
+Dir.glob(require_pattern).each do |f|
+  next if f.end_with?('/main.rb')
 
-    case c
-    when 'w'
-      y -= 1
-    when 'a'
-      x -= 1
-    when 's'
-      y += 1
-    when 'd'
-      x += 1
-    when 'c'
-      exit
-    end
+  require_relative f.gsub("#{root_dir}/", '')
+end
 
-    puts "#{x}, #{y}"
+x = 1
+y = 1
 
-    map = <<ROOM
+while (c = $stdin.getch)
+  puts "\e[H\e[2J" # clear display
+
+  case c
+  when 'w'
+    y -= 1
+  when 'a'
+    x -= 1
+  when 's'
+    y += 1
+  when 'd'
+    x += 1
+  when 'c'
+    exit
+  end
+
+  puts "#{x}, #{y}"
+
+  map = <<ROOM
   #---#
   |###|
   |###|
@@ -30,10 +39,7 @@ def main
   #---#
 ROOM
 
-    plane = map.gsub(' ', '').split("\n").map(&:chars)
-    plane[y][x] = '@'
-    puts plane.map { |row| row }.map(&:join).join("\n")
-  end
+  plane = map.gsub(' ', '').split("\n").map(&:chars)
+  plane[y][x] = '@'
+  puts plane.map { |row| row }.map(&:join).join("\n")
 end
-
-main
