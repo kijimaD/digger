ed25a735518c6d267d6b9b3d0bd64653c0c6cd03
kijima <norimaking777@gmail.com>
2021-06-24 10:29:09 +0900

    Fix test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../misc/game_window'
 require_relative '../game_state/game_state'
 require_relative '../game_state/field_state'
+require_relative '../entities/map'
+require_relative '../entities/character'
 
-RSpec.describe 'Simulate main' do
+RSpec.describe 'Simulate running main' do
   it 'Can run' do
-    GameWindow.new
+    $game = GameWindow.new
     GameState.switch(FieldState.instance)
   end
 end
