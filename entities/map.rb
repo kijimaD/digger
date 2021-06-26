5a4061ae6f30feafd6cb3288ff070631ba38c932
kijima <norimaking777@gmail.com>
2021-06-26 18:17:51 +0900

    Refactor: read file

--- entities/map.rb
+++ entities/map.rb
@@ -1,12 +1,18 @@
 # Field map class.
 class Map
   def initialize(file)
-    @text_file = File.read(Utils.media_path(file))
+    @text = File.read(Utils.media_path(file))
+    @map = text_to_array
   end
 
   def map_with_player(character)
-    new_map = @text_file.split("\n").map(&:chars)
+    new_map = text_to_array
     new_map[character.y][character.x] = '@'
     new_map.map { |row| row }.map(&:join).join("\n")
+  end
+  private
+
+  def text_to_array
+    @text.split("\n").map(&:chars)
   end
 end
