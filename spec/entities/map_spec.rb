fe66e9f2f90c953458ae980958df22addd9f45b0
kijima <norimaking777@gmail.com>
2021-06-26 10:18:43 +0900

    Fix: test

--- spec/entities/map_spec.rb
+++ spec/entities/map_spec.rb
@@ -4,30 +4,29 @@ require_relative '../../entities/character'
 RSpec.describe Map do
   it '#map_with_player' do
     character = Character.new
+    map = Map.new('test_map.txt')
     test_before =
-      '#---#
-|@##|
-|###|
-|###|
-#---#'
-
-    Map.new.map_with_player(character)
-    expect(Map.new.map_with_player(character)).to eq(test_before)
+      '#####
+#@  #
+#   #
+#   #
+#####'
+    expect(map.map_with_player(character)).to eq(test_before)
 
     character.right
     test_after =
-      '#---#
-|#@#|
-|###|
-|###|
-#---#'
-    expect(Map.new.map_with_player(character)).to eq(test_after)
+      '#####
+# @ #
+#   #
+#   #
+#####'
+    expect(map.map_with_player(character)).to eq(test_after)
 
     character.up
     character.down
-    expect(Map.new.map_with_player(character)).to eq(test_after)
+    expect(map.map_with_player(character)).to eq(test_after)
 
     character.left
-    expect(Map.new.map_with_player(character)).to eq(test_before)
+    expect(map.map_with_player(character)).to eq(test_before)
   end
 end
