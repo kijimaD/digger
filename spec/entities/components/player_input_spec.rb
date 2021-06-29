2b58c627706dc73056df731ce0237af2c9830ff7
kijima <norimaking777@gmail.com>
2021-06-30 00:03:13 +0900

    Test: add

---
+++ spec/entities/components/player_input_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../../../entities/components/player_input'
+
+RSpec.describe PlayerInput do
+  let(:object_pool) { ObjectPool.new }
+  let(:input) { PlayerInput.new(object_pool) }
+  let(:character) { Character.new(object_pool, input, 1, 1) }
+
+  it '#control' do
+    input.control(character)
+    expect(input.object).to eq character
+  end
+
+  it 'button_down' do
+    expect { input.button_down('c') }.to raise_error SystemExit
+  end
+end
