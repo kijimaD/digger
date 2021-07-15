e649e3d50a1655355f69d7d45c42f6934c89e232
kijima <norimaking777@gmail.com>
2021-07-16 00:57:32 +0900

    Refactor: rename argument

--- entities/terrain.rb
+++ entities/terrain.rb
@@ -2,10 +2,10 @@
 
 # World is composed by terrain.
 class Terrain
-  attr_reader :symbol, :passable, :name
+  attr_reader :id, :symbol, :passable
 
-  def initialize(name, symbol, passable)
-    @name = name
+  def initialize(id, symbol, passable)
+    @id = id
     @symbol = symbol
     @passable = passable
   end
