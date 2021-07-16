73349c0fe9df17a14ef7a7a9b257b126b3bbb9d2
kijima <norimaking777@gmail.com>
2021-07-16 10:35:12 +0900

    Lint: fix

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe 'Main' do
   let(:field_state) { FieldState.instance }
   # let(:field_state) { FieldState.send(:new) }
 
-  before { $game = nil }
+  before { $game = GameWindow.new }
 
   it 'Can run' do
-    $game = GameWindow.new
     GameState.switch(field_state)
     100.times do
       field_state.execute = true
