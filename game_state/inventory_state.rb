a35c0a9a315a119ac78d5c577de4328d15162aa3
kijima <norimaking777@gmail.com>
2021-07-17 19:28:02 +0900

    Add: inventory_state item tab

--- game_state/inventory_state.rb
+++ game_state/inventory_state.rb
@@ -5,6 +5,11 @@ class InventoryState < GameState
   include Singleton
   attr_accessor :field_state
 
+  def initialize
+    super
+    @cursor_x = 0
+  end
+
   def enter; end
 
   def leave; end
@@ -14,11 +19,37 @@ class InventoryState < GameState
     $game.window.setpos(1, 1)
     $game.window.addstr('Inventory')
 
-    Inventory.instance.items.each.with_index(1) do |item, i|
-      $game.window.setpos(1 + i, 1)
-      $game.window.addstr(item.type.name)
-      $game.window.setpos(1 + i, 20)
-      $game.window.addstr(item.type.description)
+    case @cursor_x
+    when 0
+      $game.window.setpos(1, 20)
+      $game.window.addstr('→アイテム')
+      $game.window.setpos(1, 30)
+      $game.window.addstr('素材')
+    when 1
+      $game.window.setpos(1, 20)
+      $game.window.addstr('アイテム')
+      $game.window.setpos(1, 30)
+      $game.window.addstr('→素材')
+    end
+
+    case @cursor_x
+    when 0
+      Inventory.instance.items.each.with_index(1) do |item, i|
+        $game.window.setpos(2 + i, 1)
+        $game.window.addstr(item.type.name)
+        $game.window.setpos(2 + i, 20)
+        $game.window.addstr(item.type.description)
+      end
+    when 1
+      materials = FieldState.instance.item_type_pool.types.select { |t| t.category == :material }
+      materials.each.with_index(1) do |type, i|
+        $game.window.setpos(2 + i, 1)
+        $game.window.addstr(type.name)
+        $game.window.setpos(2 + i, 20)
+        $game.window.addstr(type.description)
+        $game.window.setpos(2 + i, 50)
+        $game.window.addstr(type.count.to_s)
+      end
     end
   end
   # rubocop:enable Metrics/AbcSize
@@ -27,6 +58,10 @@ class InventoryState < GameState
 
   def button_down(char)
     case char
+    when 'a'
+      @cursor_x -= 1 if @cursor_x > 0
+    when 'd'
+      @cursor_x += 1 if @cursor_x < 1
     when 'e'
       GameState.switch(@field_state)
     when 'c'
