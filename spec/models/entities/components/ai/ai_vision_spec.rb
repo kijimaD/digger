bfe14a21154257f3e2782e072189f8e7cb0fbdde
kijima <norimaking777@gmail.com>
2021-07-21 10:34:28 +0900

    Test: fix fail test

--- spec/models/entities/components/ai/ai_vision_spec.rb
+++ spec/models/entities/components/ai/ai_vision_spec.rb
@@ -13,18 +13,19 @@ RSpec.describe AiVision do
 
   describe '#closest_player' do
     it 'choose most close player' do
-      ai_vision = described_class.new(GameObject.new(object_pool, 1, 1), object_pool, 4)
-
       _player_far = Character.new(object_pool, player_input, 5, 5)
       player_close = Character.new(object_pool, player_input, 2, 2)
+
+      ai_vision = described_class.new(GameObject.new(object_pool, 1, 1), object_pool, 4)
 
       expect(ai_vision.closest_player).to eq(player_close)
     end
 
     it 'exclude AI character' do
+      Character.new(object_pool, ai_input, 2, 2)
+
       ai_vision = described_class.new(GameObject.new(object_pool, 1, 1), object_pool, 4)
 
-      Character.new(object_pool, ai_input, 2, 2)
       expect(ai_vision.closest_player).to eq(nil)
     end
   end
