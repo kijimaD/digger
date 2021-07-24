0aa5af969477cfa2b0d462767882b6844c515dfb
kijima <norimaking777@gmail.com>
2021-07-24 19:51:29 +0900

    Add: monster pool

--- entities/party.rb
+++ entities/party.rb
@@ -4,13 +4,15 @@
 class Party
   include Singleton
   attr_accessor :inventory
-  attr_reader :members
+  attr_reader :members, :member_pool, :monster_pool
 
   def initialize
     @inventory = Inventory.new
     @member_pool = generate_members
     @members = []
     @members << @member_pool.find { |m| m.type.id == 'ishihara' }
+
+    @monster_pool = generate_monsters
   end
 
   def generate_members
@@ -20,5 +22,18 @@ class Party
       members << Member.new(type)
     end
     members
+  end
+
+  def generate_monsters
+    monsters = []
+    categories = %w[artillery dog dragon flag insect robot slime sub_human tank ufo]
+    categories.each do |category|
+      Utils.load_json("battle/#{category}").each do |obj|
+        obj[:category] = category
+        type = MonsterType.new(obj)
+        monsters << Monster.new(type)
+      end
+    end
+    monsters
   end
 end
--- spec/models/entities/party_spec.rb
+++ spec/models/entities/party_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Party do
     it 'access members' do
       expect(party.members.length).to eq(1)
     end
+
+    it 'access monster_pool' do
+      expect(party.monster_pool.length > 30).to be(true)
+    end
   end
 end
